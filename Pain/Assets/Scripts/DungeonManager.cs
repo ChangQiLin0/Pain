3eacf7a0990aa4008c36b1438582a3d6f163db60
ChangQiLin0 <118544377+ChangQiLin0@users.noreply.github.com>
2023-10-28 20:56:52 +0100

    procedural gen part 2
    -either 80% or 0% done
    -a bit of crashing
    -some doors face other walls
    -some rooms arent closed off

--- Pain/Assets/Scripts/DungeonManager.cs
+++ Pain/Assets/Scripts/DungeonManager.cs
@@ -15,26 +15,36 @@ public class DungeonManager : MonoBehaviour
     public Tile[] closedDoorTiles; // store all closed door tiles
     public int roomsRequired; // number of rooms that should spawn 
     public int roomsSpawned; // number of rooms currently spawned
-    public bool hasUnconnectedRooms; // if a room has a door not connected to any other rooms
 
     public GameObject[] spawnRooms; // store all rooms which are spawn rooms
-    public GameObject[] roomL; // rooms with left doors 
-    public GameObject[] roomR; // rooms with right doors 
-    public GameObject[] roomU; // rooms with doors going up
-    public GameObject[] roomD; // rooms with doors going down
+    public GameObject[] rooms; // store all room prefabs
 
-    public string[,] occupiedGrid = new string[9,9]; // 2D array -9x9 grid which shows which cells have a room/type of room e.g. LR (left+right doors)
+
+    public string[,] occupiedGrid = new string[15,15]; // 2D array -21x21 grid which shows which cells have a room/type of room e.g. LR (left+right doors)
     // if contains "Req" + "L/R/U/D" it means its requires that
 
     public void Start()
     {
-        GameObject room = Instantiate(tilePrefab, new Vector2(4*26, 4*26), quaternion.identity); // create spawn at 4,4 which is at (104,104)
-        occupiedGrid[4,4] = "LRUD"; // indicate new spawned room has doors facing in all directions
+
+        for (int i = 0; i < 15; i++) // loop from 0-20 inclusive
+        {
+            occupiedGrid[i, 0] = "boarder"; // bottom horizontal
+            occupiedGrid[0, i] = "boarder"; // left vertical 
+            occupiedGrid[i, 14] = "boarder"; // top horizontal
+            occupiedGrid[14, i] = "boarder"; // right vertical
+        }
+
+        GameObject room = Instantiate(tilePrefab, new Vector2(8*26, 8*26), quaternion.identity); // create spawn at 4,4 which is at (104,104)
+        occupiedGrid[8,8] = "LRUD"; // indicate new spawned room has doors facing in all directions
+        occupiedGrid[7,8] = "roomR";
+        occupiedGrid[9,8] = "roomL";
+        occupiedGrid[11,7] = "roomU";
+        occupiedGrid[11,9] = "roomD";
+
+
         room.transform.SetParent(tilemap.transform);
 
-        occupiedGrid[3,4] = "RoomL"; // [temp for testing] indicate there is a room at 2,4 with a right door open
-
-        room.GetComponent<DungeonRoom>().gridPos = new Vector3Int(4,4); // set location on grid
+        room.GetComponent<DungeonRoom>().gridPos = new Vector3Int(8,8); // set location on grid
         room.GetComponent<DungeonRoom>().CreateNextRoom(); // call create room method
 
         // create spawn room at centre
--- Pain/Assets/Scripts/DungeonRoom.cs
+++ Pain/Assets/Scripts/DungeonRoom.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -93,139 +93,231 @@ public class DungeonRoom : MonoBehaviour
         dungeonManager = transform.parent.GetComponent<DungeonManager>(); // get updated instance of dungeon manager
         dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y] = room.GetComponent<DungeonRoom>().doorDirections; // replace previous data with door direction data e.g. "RLD"
         
-        if (dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] != null && dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y].Contains("Room"))
+        if (room.GetComponent<DungeonRoom>().doorDirections.Contains("L"))
         {
-            dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] += "R"; // requires a right door
+            if (dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] == null)
+            {
+                dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] = "roomR"; // use key word "room" and add right to its requirement
+            }
+            else if (doorDirections.Contains("L") && dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y].Contains("room"))
+            {
+                dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] += "R"; // add right door to requirements
+            }
         }
-        else if (dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] != null && dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] == null)
+        if (room.GetComponent<DungeonRoom>().doorDirections.Contains("R"))
         {
-            dungeonManager.occupiedGrid[newGridPos.x -1, newGridPos.y] += "RoomR"; // add room keyword + direction required
+            if (dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] == null)
+            {
+                dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] = "roomL"; // use key word "room" and add left to its requirement
+            }
+            else if (doorDirections.Contains("R") && dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y].Contains("room"))
+            {
+                dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] += "L"; // add left door to requirements
+            }
         }
+        if (room.GetComponent<DungeonRoom>().doorDirections.Contains("U"))
+        {
+            if (dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] == null)
+            {
+                dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] = "roomD"; // use key word "room" and add left to its requirement
+            }
+            else if (doorDirections.Contains("U") && dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1].Contains("room"))
+            {
+                dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] += "D"; // add left door to requirements
+            }
+        }
+        if (room.GetComponent<DungeonRoom>().doorDirections.Contains("D"))
+        {
+            if (dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] == null)
+            {
+                dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] = "roomU"; // use key word "room" and add left to its requirement
+            }
+            else if (doorDirections.Contains("D") && dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1].Contains("room"))
+            {
+                dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] += "U"; // add left door to requirements
+            }
+        }
+    }
+    private bool CorrectRoom(Vector3Int pos, string doorDirections)
+    {
+        dungeonManager = transform.parent.GetComponent<DungeonManager>(); // get updated instance of dungeon manager
+        bool returnBool = false;
+        foreach (char letter in doorDirections)
+        {
+            if (doorDirections.Contains("L"))
+            {
+                if (dungeonManager.occupiedGrid[pos.x -1, pos.y] == null || dungeonManager.occupiedGrid[pos.x -1, pos.y].Contains("R")) // if cell is empty or has path pointing back
+                {
+                    returnBool = true;
+                }
+                else
+                {
+                    returnBool = false;
+                }
+            }
+            if (doorDirections.Contains("R"))
+            {
+                if (dungeonManager.occupiedGrid[pos.x +1, pos.y] == null || dungeonManager.occupiedGrid[pos.x +1, pos.y].Contains("L")) // if cell is empty or has path pointing back
+                {
+                    returnBool = true;
+                }
+                else
+                {
+                    returnBool = false;
+                }
+            }
+            if (doorDirections.Contains("U"))
+            {
+                if (dungeonManager.occupiedGrid[pos.x, pos.y +1] == null || dungeonManager.occupiedGrid[pos.x, pos.y +1].Contains("D")) // if cell is empty or has path pointing back
+                {
+                    returnBool = true;
+                }
+                else
+                {
+                    returnBool = false;
+                }
+            }
+            if (doorDirections.Contains("D"))
+            {
+                if (dungeonManager.occupiedGrid[pos.x, pos.y -1] == null || dungeonManager.occupiedGrid[pos.x, pos.y -1].Contains("U")) // if cell is empty or has path pointing back
+                {
+                    returnBool = true;
+                }
+                else
+                {
+                    returnBool = false;
+                }
+            }
+        }
+        return returnBool;
+    }
 
-        if (dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] != null && dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y].Contains("Room"))
+    public void InstantiateRoom(Vector3Int newGridPos, string requiredDirections)
+    {
+        dungeonManager = transform.parent.GetComponent<DungeonManager>(); // get updated instance of dungeon manager
+        if (dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y] != null && dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y].Contains("room"))
         {
-            dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] += "L"; // requires a right door
+            bool validRoom = false;
+            while (!validRoom)
+            {
+                int validCounter = 0; // always set validcounter to 0 on each iteration
+                GameObject potentialRoom = dungeonManager.rooms[Random.Range(0, dungeonManager.rooms.Length)]; // get and store potential room 
+                foreach (char letter in potentialRoom.GetComponent<DungeonRoom>().doorDirections) // loops through each letter in doordirection string
+                {
+                    if (requiredDirections.Contains(letter.ToString()) && letter.ToString() != "") // checks if given potential rooms direction matches required
+                    {
+                        validCounter += 1; // increment counter by 1
+                    }
+                }
+                if (validCounter == requiredDirections.Length && CorrectRoom(newGridPos, potentialRoom.GetComponent<DungeonRoom>().doorDirections)) // if counter shows that room contains all required directions
+                {
+                    GameObject room = Instantiate(potentialRoom, new Vector3Int(newGridPos.x*26, newGridPos.y*26, 0), Quaternion.identity); // instantiate room in tilemap grid
+                    room.transform.parent = dungeonManager.transform; // set parent
+                    dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y] = room.GetComponent<DungeonRoom>().doorDirections; // replace previous data with door direction data e.g. "RLD"
+                    UpdateOccupiedGrid(newGridPos, room);
+            
+                    if (room.GetComponent<DungeonRoom>().doorDirections.Length > 1)
+                    {
+                        dungeonManager = transform.parent.GetComponent<DungeonManager>(); // get updated instance of dungeon manager
+                        room.GetComponent<DungeonRoom>().CreateNextRoom();
+                    }
+                    validRoom = true;
+                }
+            }
         }
-        else if (dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] != null && dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] == null)
-        {
-            dungeonManager.occupiedGrid[newGridPos.x +1, newGridPos.y] += "RoomL"; // add room keyword + direction required
-        }
-
-        if (dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] != null && dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1].Contains("Room"))
-        {
-            dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] += "U"; // requires a right door
-        }
-        else if (dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] != null && dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] == null)
-        {
-            dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y -1] += "RoomU"; // add room keyword + direction required
-        }
-
-        if (dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] != null && dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1].Contains("Room"))
-        {
-            dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] += "D"; // requires a right door
-        }
-        else if (dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] != null && dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] == null)
-        {
-            dungeonManager.occupiedGrid[newGridPos.x, newGridPos.y +1] += "RoomD"; // add room keyword + direction required 
-        }
-
-        room.GetComponent<DungeonRoom>().CreateNextRoom(); // continue making rooms if possible
     }
 
 
     public void CreateNextRoom()
     {
-        string requiredDirections = ""; // stores which paths next spawned room must have
+        dungeonManager = transform.parent.GetComponent<DungeonManager>(); // get updated instance of dungeon manager
+        string requiredDirections; // stores which paths next spawned room must have
         if (doorDirections.Contains("L")) // spawn a room on the left
         {
             requiredDirections = CheckSurroundingRooms(gridPos, "L");
-            dungeonManager.occupiedGrid[gridPos.x, gridPos.y] = doorDirections; // remove "Room" keyword from grid to prevent overlap
-
-            Debug.Log(requiredDirections);
-            Vector3Int newGridPos = new Vector3Int(gridPos.x - 1, gridPos.y); // create new vector3 pos of where the room should spawn at
-            GameObject room = Instantiate(dungeonManager.roomR[0], new Vector3Int(newGridPos.x*26, newGridPos.y*26, 0), quaternion.identity); // instantiate room in tilemap grid
-            room.transform.parent = dungeonManager.transform; // set parent
             
-            UpdateOccupiedGrid(newGridPos, room);
-            
+            Vector3Int newGridPos = new Vector3Int(gridPos.x -1, gridPos.y); // create new vector3 pos of where the room should spawn at
+            InstantiateRoom(newGridPos, requiredDirections); // call method which will verify and instantiate rooms
         }
         if (doorDirections.Contains("R")) // spawn a room on the right
         {
             requiredDirections = CheckSurroundingRooms(gridPos, "R");
-            Debug.Log(requiredDirections);
-            GameObject room = Instantiate(dungeonManager.roomL[0], new Vector3Int((gridPos.x + 1)*26, gridPos.y*26, 0), quaternion.identity);
-            room.transform.parent = dungeonManager.transform;
+
+            Vector3Int newGridPos = new Vector3Int(gridPos.x +1, gridPos.y); // create new vector3 pos of where the room should spawn at
+            InstantiateRoom(newGridPos, requiredDirections); // call method which will verify and instantiate rooms
         }
         if (doorDirections.Contains("U")) // spawn a room above
         {
             requiredDirections = CheckSurroundingRooms(gridPos, "U");
-            Debug.Log(requiredDirections);
-            GameObject room = Instantiate(dungeonManager.roomD[0], new Vector3Int(gridPos.x*26, (gridPos.y + 1)*26, 0), quaternion.identity);
-            room.transform.parent = dungeonManager.transform;
+
+            Vector3Int newGridPos = new Vector3Int(gridPos.x, gridPos.y +1); // create new vector3 pos of where the room should spawn at
+            InstantiateRoom(newGridPos, requiredDirections); // call method which will verify and instantiate rooms
         }
         if (doorDirections.Contains("D")) // spawn a room below
         {
             requiredDirections = CheckSurroundingRooms(gridPos, "D");
-            Debug.Log(requiredDirections);
-            GameObject room = Instantiate(dungeonManager.roomU[0], new Vector3Int(gridPos.x*26,(gridPos.y - 1)*26, 0), quaternion.identity);
-            room.transform.parent = dungeonManager.transform;
+
+            Vector3Int newGridPos = new Vector3Int(gridPos.x, gridPos.y -1); // create new vector3 pos of where the room should spawn at
+            InstantiateRoom(newGridPos, requiredDirections); // call method which will verify and instantiate rooms
         }
-        
     }
 
     public string CheckSurroundingRooms(Vector3Int currentPos, string checkDirection) // returns what directions are required
     {
-        string returnString = "";
+        string validDirections = ""; // contains all direction which is free
+        
         dungeonManager = transform.parent.GetComponent<DungeonManager>(); // get updated instance of dungeon manager
         if (checkDirection == "L")
         {
-            Vector3Int leftPos = new Vector3Int(currentPos.x -= 1, currentPos.y); // subtract 1 from x coord to get left position
-            if (dungeonManager.occupiedGrid[leftPos.x, leftPos.y] != null && dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Contains("Room"))
+            Vector3Int leftPos = new Vector3Int(currentPos.x - 1, currentPos.y); // subtract 1 from x coord to get left position
+            if (dungeonManager.occupiedGrid[leftPos.x, leftPos.y] != null && dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Contains("room"))
             {
-                returnString += dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
+                validDirections += dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
             }
         }
         if (checkDirection == "R")
         {
-            Vector3Int leftPos = new Vector3Int(currentPos.x += 1, currentPos.y); // add 1 to x coord to get right position
-            if (dungeonManager.occupiedGrid[leftPos.x, leftPos.y] != null && dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Contains("Room"))
+            Vector3Int rightPos = new Vector3Int(currentPos.x + 1, currentPos.y); // add 1 to x coord to get right position
+            if (dungeonManager.occupiedGrid[rightPos.x, rightPos.y] != null && dungeonManager.occupiedGrid[rightPos.x, rightPos.y].Contains("room"))
             {
-                returnString += dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
+                validDirections += dungeonManager.occupiedGrid[rightPos.x, rightPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
             }
         }
         if (checkDirection == "U")
         {
-            Vector3Int leftPos = new Vector3Int(currentPos.x, currentPos.y += 1); // add 1 to y coord to get up position
-            if (dungeonManager.occupiedGrid[leftPos.x, leftPos.y] != null && dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Contains("Room"))
+            Vector3Int upPos = new Vector3Int(currentPos.x, currentPos.y + 1); // add 1 to y coord to get up position
+            if (dungeonManager.occupiedGrid[upPos.x, upPos.y] != null && dungeonManager.occupiedGrid[upPos.x, upPos.y].Contains("room"))
             {
-                returnString += dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
+                validDirections += dungeonManager.occupiedGrid[upPos.x, upPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
             }
         }
         if (checkDirection == "D")
         {
-            Vector3Int leftPos = new Vector3Int(currentPos.x, currentPos.y -= 1); // subtract 1 from x coord to get down position
-            if (dungeonManager.occupiedGrid[leftPos.x, leftPos.y] != null && dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Contains("Room"))
+            Vector3Int downPos = new Vector3Int(currentPos.x, currentPos.y - 1); // subtract 1 from x coord to get down position
+            if (dungeonManager.occupiedGrid[downPos.x, downPos.y] != null && dungeonManager.occupiedGrid[downPos.x, downPos.y].Contains("room"))
             {
-                returnString += dungeonManager.occupiedGrid[leftPos.x, leftPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
+                validDirections += dungeonManager.occupiedGrid[downPos.x, downPos.y].Substring(4); // returns everything past Req e.g. "LD" (left + down)
             }
         }
 
-        switch (checkDirection) // return oposite of what checkDirection was
+        if (validDirections == "")
         {
-            case "L":
-                returnString += "R";
-                break;
-            case "R":
-                returnString += "L";
-                break;
-            case "U":
-                returnString += "D";
-                break;
-            case "D":
-                returnString += "U";
-                break;
+            switch (checkDirection) // return oposite of what checkDirection was
+            {
+                case "L":
+                    validDirections += "R";
+                    break;
+                case "R":
+                    validDirections += "L";
+                    break;
+                case "U":
+                    validDirections += "D";
+                    break;
+                case "D":
+                    validDirections += "U";
+                    break;
+            }
         }
-        
-        return returnString;
+        return validDirections;
     }
 }
